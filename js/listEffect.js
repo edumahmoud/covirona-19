8500958e8f0d3000dacb6d265664b8f167405331
edumahmoud <144780054+edumahmoud@users.noreply.github.com>
2024-05-17 15:27:58 +0300

    publish

--- js/listEffect.js
+++ js/listEffect.js
@@ -5,14 +5,14 @@
 */
 let listItems = document.querySelectorAll('.listItem');
 
-    listItems.forEach(item => {
-        item.addEventListener('click', function(){
-            // إزالة اسم الكلاس "active" من جميع العناصر في القائمة
-            listItems.forEach(listItem => {
+listItems.forEach(item => {
+    item.addEventListener('click', function(){
+        // إزالة اسم الكلاس "active" من جميع العناصر في القائمة
+        listItems.forEach(listItem => {
             listItem.classList.remove('active');
-            });
-
-            // إضافة اسم الكلاس "active" إلى العنصر الحالي
-            this.classList.add('active');
         });
+
+        // إضافة اسم الكلاس "active" إلى العنصر الحالي
+        this.classList.add('active');
     });
+});
